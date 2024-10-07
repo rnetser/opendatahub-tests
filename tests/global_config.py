9d40733485e8580de221f55d60d6bb44a9ffe1d2
rnetser <rnetser@redhat.com>
2024-10-07 13:48:21 +0300

    add runtime class

--- tests/global_config.py
+++ tests/global_config.py
@@ -4,16 +4,12 @@ from typing import Optional, Any, Dict
 global config
 config: Dict[Any, Any] = {}
 
-no_unprivileged_client: bool = False
 # AWS
 aws_secret_access_key: Optional[str] = os.environ.get("AWS_SECRET_ACCESS_KEY", "aws_secret_key")
 aws_access_key_id: Optional[str] = os.environ.get("AWS_ACCESS_KEY_ID", "aws_access_key")
 
-# S3
+# S3 Buckets
 ci_s3_bucket_name: str = "ci-bucket"
-model_s3_bucket_name: str = "s3-bucket"
-model_s3_bucket_region: str = "us-east-1"
-model_s3_endpoint: str = f"https://{model_s3_bucket_region}.amazonaws.com/"
 
 for _dir in dir():
     val = locals()[_dir]
