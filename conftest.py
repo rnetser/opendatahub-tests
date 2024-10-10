2746f45d966f9933420d2adddce4dd6f0775b582
rnetser <rnetser@redhat.com>
2024-10-10 14:13:56 +0300

    Fix logger

--- conftest.py
+++ conftest.py
@@ -1,14 +1,46 @@
 import logging
+import os
+import pathlib
 import shutil
 
-from simple_logger.logger import get_logger
+from utilities.logger import separator, setup_logging
 
-from utilities.pytest_utils import (
-    separator,
-)
 
+LOGGER = logging.getLogger(__name__)
 BASIC_LOGGER = logging.getLogger("basic")
-LOGGER = get_logger(__name__)
+
+
+def pytest_addoption(parser):
+    log_group = parser.getgroup(name="PytestLog")
+    log_group.addoption(
+        "--pytest-log-file",
+        help="Path to pytest log file",
+        default="pytest-tests.log",
+    )
+
+
+def pytest_sessionstart(session):
+    tests_log_file = session.config.getoption("pytest_log_file")
+    if os.path.exists(tests_log_file):
+        pathlib.Path(tests_log_file).unlink()
+
+    session.config.option.log_listener = setup_logging(
+        log_file=tests_log_file,
+        log_level=session.config.getoption("log_cli_level") or logging.INFO,
+    )
+
+
+def pytest_fixture_setup(fixturedef, request):
+    LOGGER.info(f"Executing {fixturedef.scope} fixture: {fixturedef.argname}")
+
+
+def pytest_runtest_setup(item):
+    BASIC_LOGGER.info(f"\n{separator(symbol_='-', val=item.name)}")
+    BASIC_LOGGER.info(f"{separator(symbol_='-', val='SETUP')}")
+
+
+def pytest_runtest_teardown(item):
+    BASIC_LOGGER.info(f"{separator(symbol_='-', val='TEARDOWN')}")
 
 
 def pytest_report_teststatus(report, config):
@@ -27,19 +59,6 @@ def pytest_report_teststatus(report, config):
             BASIC_LOGGER.info(f"\nTEST: {test_name} [{when}] STATUS: \033[0;31mERROR\033[0m")
         else:
             BASIC_LOGGER.info(f"\nTEST: {test_name} STATUS: \033[0;31mFAILED\033[0m")
-
-
-def pytest_fixture_setup(fixturedef, request):
-    LOGGER.info(f"Executing {fixturedef.scope} fixture: {fixturedef.argname}")
-
-
-def pytest_runtest_setup(item):
-    BASIC_LOGGER.info(f"\n{separator(symbol_='-', val=item.name)}")
-    BASIC_LOGGER.info(f"{separator(symbol_='-', val='SETUP')}")
-
-
-def pytest_runtest_teardown(item):
-    BASIC_LOGGER.info(f"{separator(symbol_='-', val='TEARDOWN')}")
 
 
 def pytest_sessionfinish(session, exitstatus):
