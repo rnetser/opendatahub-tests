bcca13d79da5fb46d51f8318dce43431afa03e80
rnetser <rnetser@redhat.com>
2024-10-22 12:23:32 +0300

    add kserver serverless auth tests

--- tests/model_serving/model_server/authentication/test_kserve_token_authentication.py
+++ tests/model_serving/model_server/authentication/test_kserve_token_authentication.py
@@ -1,6 +1,12 @@
 class TestKserveTokenAuthentication:
-    def test_single_model_authentication(self):
+    def test_model_authentication(self):
         pass
 
     def test_block_cross_model_authentication(self):
+        pass
+
+    def test_disabled_model_authentication(self):
+        pass
+
+    def test_re_enabled_model_authentication(self):
         pass
