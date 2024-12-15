c584628d4ef9e9872bef77625bc242ce4452c540
rnetser <rnetser@redhat.com>
2024-12-15 17:54:10 +0200

    Add RawDeployment route tests

--- tests/model_serving/model_server/raw_deployment/conftest.py
+++ tests/model_serving/model_server/raw_deployment/conftest.py
@@ -27,3 +27,25 @@ def patched_isvc_visibility_annotation(
         predictor_pod.wait_deleted()
 
         yield http_s3_inference_service
+
+
+@pytest.fixture()
+def patched_grpc_isvc_visibility_annotation(
+    request: FixtureRequest, admin_client: DynamicClient, grpc_s3_inference_service: InferenceService
+) -> InferenceService:
+    with ResourceEditor(
+        patches={
+            grpc_s3_inference_service: {
+                "metadata": {
+                    "annotations": {"networking.kserve.io/visibility": request.param["visibility"]},
+                }
+            }
+        }
+    ):
+        predictor_pod = get_pods_by_isvc_label(
+            client=admin_client,
+            isvc=grpc_s3_inference_service,
+        )[0]
+        predictor_pod.wait_deleted()
+
+        yield grpc_s3_inference_service
--- tests/model_serving/model_server/raw_deployment/test_raw_deployment.py
+++ tests/model_serving/model_server/raw_deployment/test_raw_deployment.py
@@ -19,7 +19,13 @@ pytestmark = pytest.mark.usefixtures("valid_aws_config")
     indirect=True,
 )
 class TestRawDeployment:
+    def test_default_visibility_value(self, http_s3_inference_service):
+        # TODO: add test cases
+        """Test default route visibility value"""
+        pass
+
     def test_rest_raw_deployment_internal_route(self, http_s3_inference_service):
+        """Test HTTP inference using internal route"""
         verify_inference_response(
             inference_service=http_s3_inference_service,
             runtime=RuntimeQueryKeys.CAIKIT_TGIS_RUNTIME,
@@ -33,17 +39,63 @@ class TestRawDeployment:
         "patched_isvc_visibility_annotation",
         [
             pytest.param(
-                {"visibility": "cluster-local"},
+                {"visibility": "external"},
             )
         ],
         indirect=True,
     )
     def test_rest_raw_deployment_external_route(self, patched_isvc_visibility_annotation):
+        """Test HTTP inference using external route"""
         verify_inference_response(
             inference_service=patched_isvc_visibility_annotation,
             runtime=RuntimeQueryKeys.CAIKIT_TGIS_RUNTIME,
             inference_type=Inference.ALL_TOKENS,
             protocol=Protocols.HTTP,
+            model_name=ModelFormat.CAIKIT,
+            use_default_query=True,
+        )
+
+    @pytest.mark.parametrize(
+        "patched_isvc_visibility_annotation",
+        [
+            pytest.param(
+                {"visibility": "local-cluster"},
+            )
+        ],
+        indirect=True,
+    )
+    def disable_external_route(self, patched_isvc_visibility_annotation):
+        """Test HTTP inference fails when using external route after it was disabled"""
+        # TODO: add test
+        pass
+
+    def test_grpc_raw_deployment_internal_route(self, grpc_s3_inference_service):
+        """Test GRPC inference using internal route"""
+        verify_inference_response(
+            inference_service=grpc_s3_inference_service,
+            runtime=RuntimeQueryKeys.CAIKIT_TGIS_RUNTIME,
+            inference_type=Inference.ALL_TOKENS,
+            protocol=Protocols.GRPC,
+            model_name=ModelFormat.CAIKIT,
+            use_default_query=True,
+        )
+
+    @pytest.mark.parametrize(
+        "patched_grpc_isvc_visibility_annotation",
+        [
+            pytest.param(
+                {"visibility": "external"},
+            )
+        ],
+        indirect=True,
+    )
+    def test_grpc_raw_deployment_external_route(self, patched_grpc_isvc_visibility_annotation):
+        """Test GRPC inference using external route"""
+        verify_inference_response(
+            inference_service=patched_grpc_isvc_visibility_annotation,
+            runtime=RuntimeQueryKeys.CAIKIT_TGIS_RUNTIME,
+            inference_type=Inference.ALL_TOKENS,
+            protocol=Protocols.GRPC,
             model_name=ModelFormat.CAIKIT,
             use_default_query=True,
         )
