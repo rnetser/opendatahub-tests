9d8c531ff0ffe739a1aca60971fca5df3e1a2ef3
rnetser <rnetser@redhat.com>
2024-12-23 21:57:12 +0200

    update code

--- utilities/inference_utils.py
+++ utilities/inference_utils.py
@@ -33,11 +33,12 @@ class Inference:
         """
         self.inference_service = inference_service
         self.runtime = runtime
-        self.visibility_exposed = (
-            self.inference_service.instance.metadata.annotations.get("networking.kserve.io/visibility") == "exposed"
-        ) or self.inference_service.instance.metadata.annotations.get(
-            "networking.knative.dev/visibility"
-        ) != "cluster-local"
+
+        self.visibility_exposed = self.inference_service.labels and (
+            self.inference_service.instance.metadata.labels.get("networking.kserve.io/visibility") == "exposed"
+            or self.inference_service.instance.metadata.labels.get("networking.knative.dev/visibility")
+            != "cluster-local"
+        )
         self.inference_url = self.get_inference_url()
 
     def get_inference_url(self) -> str:
