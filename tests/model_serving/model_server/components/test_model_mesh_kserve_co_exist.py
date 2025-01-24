f2cc434f7048f8f16a31193e4f1b19b38d916111
rnetser <rnetser@redhat.com>
2025-01-24 15:11:14 +0200

    increase timeout

--- tests/model_serving/model_server/components/test_model_mesh_kserve_co_exist.py
+++ tests/model_serving/model_server/components/test_model_mesh_kserve_co_exist.py
@@ -5,14 +5,15 @@ from tests.model_serving.model_server.components.constants import KSERVE
 from utilities.constants import ComponentManagementState
 
 LOGGER = get_logger(name=__name__)
+TIMEOUT = 6 * 60
 
 
 @pytest.mark.usefixtures("managed_modelmesh_kserve_in_dsc")
 class TestKserveModelmeshCoexist:
     def test_model_mesh_state_in_dsc(self, dsc_resource):
         LOGGER.info(f"Verify model mesh state in DSC is {ComponentManagementState.MANAGED}")
-        dsc_resource.wait_for_condition(condition="ModelMeshServingReady", status="True")
+        dsc_resource.wait_for_condition(condition="ModelMeshServingReady", status="True", timeout=TIMEOUT)
 
     def test_kserve_state_in_dsc(self, dsc_resource):
         LOGGER.info(f"Verify {KSERVE} state in DSC is {ComponentManagementState.MANAGED}")
-        dsc_resource.wait_for_condition(condition="kserveReady", status="True")
+        dsc_resource.wait_for_condition(condition="kserveReady", status="True", timeout=TIMEOUT)
